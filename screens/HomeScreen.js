e9d792fe8b24e191aa4ea8eb995002d6f1e1a5ac
Bronson Duran <brons@outlook.com>
2017-05-02 15:19:17 -0700

    first time user

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -61,7 +61,7 @@ export default class HomeScreen extends React.Component {
 
   // Setup what to do with the user information.
   userFirstTimeCallback(user, exists) {
-    if (true) {
+    if (!exists) {
       Database.setUserData(user.uid, {
         name: user.displayName,
         photoURL: user.photoURL,
