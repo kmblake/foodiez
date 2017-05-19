23e69065d76b1cb199af4cd37e8deace8c46af65
Kent Blake <kmblake@stanford.edu>
2017-05-19 02:46:47 -0700

    Removed hosting text from confirm event screen

--- screens/ConfirmEventScreen.js
+++ screens/ConfirmEventScreen.js
@@ -128,7 +128,6 @@ export default class CreateEventScreen extends DefaultScreen {
     const d = new Date(this.state.event.date);
     const m = Moment(this.props.event.date);
     const attendingUsers = this.renderAttending();
-    const hostingText = this.renderHostingText();
     const menu = this.renderMenu();
     return (
       <ScrollView>
@@ -140,9 +139,6 @@ export default class CreateEventScreen extends DefaultScreen {
               secondaryText: m.format("ddd MMM Do h:mm a"),
           }}
         />
-        <View style={styles.textContainer}>
-          {hostingText}
-        </View>
       </Card>
       <EventCard title="Invited Friends">
         {attendingUsers}
